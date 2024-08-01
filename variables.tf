d9a13c40ec5379081119403afed9dc8bf456925d
Jerin <163454926+sanciajerin@users.noreply.github.com>
2024-07-31 21:55:31 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+ variable "instance-ami" {
+  default = "ami-04a81a99f5ec58529"
+ }
+
+ variable "instance-aws" {
+    default = "t2.micro"
+ }
+
+ variable "instance-sg" {
+    default = "sg-0d77354df6d33803f"
+ }
+
+variable "instance-count" {
+  default = 4
+}
